2ad1fc1235ce7926ff3a77df55314922ffb731ab
musgravehill <musgravehill@gmail.com>
2015-11-12 15:51:05 +0400

    pd

--- powerDown/powerDown.ino
+++ powerDown/powerDown.ino
@@ -3,6 +3,7 @@
 uint16_t counterSleep_x10 = 0;
 uint16_t counterSleep_x20 = 0;
 uint16_t counterSleep_x30 = 0;
+unsigned long t;
 
 const int ledPin =  13;
 
@@ -15,29 +16,26 @@ void setup(){
 
 void loop() {  
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
-  if(counterSleep_x10<10){
-    counterSleep_x10++;
-    task_x10();
-  }
-  else{
+  counterSleep_x10++;
+  counterSleep_x20++;
+  counterSleep_x30++;
+
+  if(counterSleep_x10 >= 10){
     counterSleep_x10 = 0;
+    //task_x10();     
+  } 
+
+  if(counterSleep_x20 >= 20){
+    counterSleep_x20 = 0;
+    //task_x20();     
+  } 
+
+  if(counterSleep_x30 >= 30){
+    counterSleep_x30 = 0;
+    task_x30();     
   }  
 
-  if(counterSleep_x20<20){
-    counterSleep_x20++;
-    task_x20();
-  }
-  else{
-    counterSleep_x20 = 0;
-  }
 
-  if(counterSleep_x30<30){
-    counterSleep_x30++;
-    task_x30();
-  }
-  else{
-    counterSleep_x30 = 0;
-  }    
 }
 
 void task_x10(){
@@ -59,18 +57,22 @@ void task_x20(){
 }
 
 void task_x30(){
-  digitalWrite(ledPin, 1);
-  delay(200);
-  digitalWrite(ledPin, 0);
-  delay(200);
-  digitalWrite(ledPin, 1);
-  delay(200);
-  digitalWrite(ledPin, 0);
-  delay(200);
-  digitalWrite(ledPin, 1);
-  delay(200);
-  digitalWrite(ledPin, 0);
-
-  Serial.println("Zzzzz...");
-  delay(500); //time to send data, then CPU sleep
+  /*digitalWrite(ledPin, 1);
+   delay(100);
+   digitalWrite(ledPin, 0);
+   delay(100);
+   digitalWrite(ledPin, 1);
+   delay(100);
+   digitalWrite(ledPin, 0);
+   delay(100);
+   digitalWrite(ledPin, 1);
+   delay(100);
+   digitalWrite(ledPin, 0);*/
+  unsigned long dt = t - millis();
+  t = millis();
+  Serial.println(dt, DEC);
+  delay(100); //time to send data, then CPU sleep
 }
+
+
+
