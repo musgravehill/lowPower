e59caafa88065f3d85e15a9866819aaa953bd8a6
musgravehill <musgravehill@gmail.com>
2015-11-12 15:15:29 +0400

    timeMachine

--- powerDown/powerDown.ino
+++ powerDown/powerDown.ino
@@ -1,45 +1,76 @@
 #include "LowPower.h"
 
-uint16_t countSleep_counter = 0;
-uint16_t countSleep = 40; //how many times sleep of 8s
+uint16_t counterSleep_x10 = 0;
+uint16_t counterSleep_x20 = 0;
+uint16_t counterSleep_x30 = 0;
 
 const int ledPin =  13;
 
 void setup(){  
+  delay(1000);
   pinMode(ledPin, OUTPUT); 
-  Serial.begin(9600);
-
-  digitalWrite(ledPin, 1);
-  delay(50);
-  digitalWrite(ledPin, 0);  
-  delay(50);
-  digitalWrite(ledPin, 1);
-  delay(50);
-  digitalWrite(ledPin, 0);  
-  delay(50);
-  digitalWrite(ledPin, 1);
-  delay(50);
-  digitalWrite(ledPin, 0);  
-  delay(50);
+  Serial.begin(9600);  
+  delay(1000);
 }
 
-void loop() {    
-  powerDwn();  
+void loop() {  
+  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+  if(counterSleep_x10<10){
+    counterSleep_x10++;
+    task_x10();
+  }
+  else{
+    counterSleep_x10 = 0;
+  }  
 
+  if(counterSleep_x20<20){
+    counterSleep_x20++;
+    task_x20();
+  }
+  else{
+    counterSleep_x20 = 0;
+  }
+
+  if(counterSleep_x30<30){
+    counterSleep_x30++;
+    task_x30();
+  }
+  else{
+    counterSleep_x30 = 0;
+  }    
+}
+
+void task_x10(){
   digitalWrite(ledPin, 1);
-  delay(900);
+  delay(200);
+  digitalWrite(ledPin, 0); 
+}
+
+void task_x20(){
+  digitalWrite(ledPin, 1);
+  delay(200);
   digitalWrite(ledPin, 0);
-  
+  delay(200);
+  digitalWrite(ledPin, 1);
+  delay(200);
+  digitalWrite(ledPin, 0);  
+
+
+}
+
+void task_x30(){
+  digitalWrite(ledPin, 1);
+  delay(200);
+  digitalWrite(ledPin, 0);
+  delay(200);
+  digitalWrite(ledPin, 1);
+  delay(200);
+  digitalWrite(ledPin, 0);
+  delay(200);
+  digitalWrite(ledPin, 1);
+  delay(200);
+  digitalWrite(ledPin, 0);
+
   Serial.println("Zzzzz...");
   delay(500); //time to send data, then CPU sleep
-}
-
-void powerDwn(){  
-  while(countSleep_counter < countSleep){
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-    //in theory: for controlling for BOD you should change FUSEs on Arduino. So, by default BOD always ON for Arduino. 
-    //But when I set BOD_OFF in code (I have not changed fuses) => arduino current consume changed from 36mkA to 14mkA O_o
-    countSleep_counter++;
-  }   
-  countSleep_counter = 0;
 }
